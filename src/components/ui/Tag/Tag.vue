9b4d5e5da7888289f91e97300cf20dbd080b6dd6
Vignesh M <m.vignes1998@gmail.com>
2023-05-19 16:10:05 +0530

    feat: Tag component modified

--- src/components/ui/Tag/Tag.vue
+++ src/components/ui/Tag/Tag.vue
@@ -1,8 +1,12 @@
 <template>
     <div v-bind="rest" :class="classNames('gc-tag', className)">
-        <slot name="prefix" :class="classNames('gc-tag-affix gc-tag-prefix', prefixClass)" />
+        <template v-for="(item, index) in defaultPrefixTags" :key="index">
+            <component :is="item"></component>
+        </template>
         <slot />
-        <slot name="suffix" :class="classNames('gc-tag-affix gc-tag-suffix', suffixClass)" />
+        <template v-for="(item, index) in defaultSuffixTags" :key="index">
+            <component :is="item"></component>
+        </template>
     </div>
 </template>
 <script>
@@ -12,12 +16,37 @@ export default {
 </script>
 <script setup>
 import classNames from 'classnames'
-import { useAttrs } from 'vue'
+import { h, useAttrs, useSlots } from 'vue'
 
-defineProps({
+const props = defineProps({
     prefixClass: String,
     suffixClass: String,
 })
+const slots = useSlots()
 
+const defaultPrefixTags =
+    slots.prefix &&
+    slots.prefix()?.map((item) => {
+        return h(
+            item.type,
+            {
+                ...item.props,
+                class: classNames('gc-tag-affix gc-tag-prefix', props.prefixClass),
+            },
+            item.children
+        )
+    })
+const defaultSuffixTags =
+    slots.suffix &&
+    slots.suffix()?.map((item) => {
+        return h(
+            item.type,
+            {
+                ...item.props,
+                class: classNames('gc-tag-affix gc-tag-suffix', props.suffixClass),
+            },
+            item.children
+        )
+    })
 const { class: className, ...rest } = useAttrs()
 </script>
